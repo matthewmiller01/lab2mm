b9e352107a5c6e16a0d94832f69ea381c7daf118
Matthew Miller <matthewmiller@Matthews-MacBook-Pro.local>
2018-09-24 20:28:31 -0400

    all requirements met

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Lab 2
+Overall Schema of Website:
+
+At the index page, the user has the option to click on any gallery they would like. Upon clicking on the gallery, a list of objects is displayed that we render using the gallery template. Each of those objects has a title. Upon clicking on the link of the title of an object, we render the object template, which lists certain things about the object like its provenance, accession year, and various other details.
+
+Code Method:
+
+Under the hood after the user clicks on a gallery, we asynchronously render the gallery template after the fetch request successfully gets us the response data that will give us a list of objects in the gallery clicked upon. We refer to all the objects in this gallery as "data.records" because this returns a JavaScript list object with each object and its variables specific to that gallery.
+
+When the user clicks on an individual object, we implement a get request to go to the route for that object page using its ID. Like with the gallery route, we also fetch all data for that specific object asynchronously. We render the object template we created and pass in all variables from the "data" about this object that we want to display in the template, which includes title, description, accessionyear, provenance, primaryimageurl, and id, all variables specified in the object API.
+
+Handling Comments:
+
+Within the object template, we also use HTML to create a form through which the user can submit comments. When a comment is submitted, we submit a post request to the route '/commenthandler/:object_id' where the latter part of the address is the actual ID of the object for which the comment was submitted. At this commenthandler route, now we push the comment into a larger JavaScript list object that holds all comments. Each object in this JavaScript object has two variables: the text of the comment itself and the ID of the object to which it belongs. We then redirect to the route for the object, which we recreate by using the object_id variable passed into the post request through the URL.
+
+Each time the route for an individual object is reached, we loop through all the comments in our larger JavaScript object list of comments called "comments" and compare the ID of each comment to the ID of the current object whose page we are on. If the IDs match, we add the comment to an individual list for this object called objectcomms and ultimately pass in this individualized list of comments to the rendered object template, where all the comments will be displayed.
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
 require('cross-fetch/polyfill');
+//using body-parser express package downloaded from online for form submission
+const bodyParser = require('body-parser');
+
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const host = '127.0.0.1';
 const port = 3000;
 
 app.set('view engine', 'ejs');
+// all templates will be saved in directory views
 app.set('views', path.join(__dirname, 'views'));
-
+//provided by Harvard Art Museums
 const API_KEY = '6a6275c0-b77e-11e8-a4d1-69890776a30b';
 
 // behavior for the index route
@@ -17,18 +24,63 @@ app.get('/', (req, res) => {
   fetch(url)
   .then(response => response.json())
   .then(data => {
+    //displays all galleries with HTML from index template
     res.render('index', {galleries: data.records});
   });
 });
-
+//uses the id number of the gallery to get the specific objects within it
 app.get('/gallery/:gallery_id', function(req, res) {
   const ids = `https://api.harvardartmuseums.org/object?gallery=${req.params.gallery_id}&apikey=${API_KEY}`;
   fetch(ids)
   .then(response => response.json())
   .then(data => {
+    //refers to the gallery template where we have HTML to list individual objects
     res.render('gallery', {objects: data.records});
   })
 });
+
+//an overall JavaScript object containing all comments as dictionaries with their text and an id number
+let comments = [];
+//gets page for the individual object based on its ID number
+app.get('/object/:object_id', function(req, res){
+  const ids = `https://api.harvardartmuseums.org/object/${req.params.object_id}?apikey=${API_KEY}`;
+  fetch(ids)
+  .then(response => response.json())
+  .then(data =>{
+    console.log("HI");
+    console.log(comments);
+    //JavaScript object that will contain comments pertaining to this individual object
+    let objectcomms = [];
+    //loops through all comments and checks if a given comment matches the ID of this object
+    comments.forEach(comment =>{
+    if (comment.id == req.params.object_id)
+      {
+          //adds it to the list of comments for this object to be displayed
+          objectcomms.push(comment.text)
+        }
+    })
+    //renders template for the individual object with all relevant variables passed in
+    let commentString = "";
+    let commNumber = 1;
+    objectcomms.forEach(comment =>{
+      commentString += commNumber + ". ";
+      commentString+= " " + comment.toString() + " ";
+      commNumber++;
+    })
+    res.render('object', {title: data.title, description: data.description, accessionyear: data.accessionyear, primaryimageurl : data.primaryimageurl, commentString: commentString, provenance: data.provenance, object_id : data.id});
+   })
+});
+//when form submitted, this handles the post request by adding the comment to list using body-parser
+app.post('/commenthandler/:object_id', function(req, res){
+  console.log(req.body);
+  //adds the comment and its object ID to the overall list of comments
+  comments.push({text: req.body.comment, id: req.params.object_id});
+  //creates and concatenates a string for the redirect URL to go back to object page
+  let address = '/object/';
+  address+= req.params.object_id;
+  //redirects to page for the individual object after adding comment for it
+  res.redirect(address);
+})
 
 app.listen(port, host, () => {
   console.log(`Server running on http://${host}:${port}/`);
